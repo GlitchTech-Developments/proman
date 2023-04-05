48f27b873a58176bb327fcd59ee76e10a364917d
Jesse Koldewijn <jesse@iungo.nl>
2023-04-05 13:36:53 +0200

    fix: stopped ignoring of build files

---
+++ bin/src/proman.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { program } from "commander";
+import { runBuild, runExec } from "./utils.js";
+program
+    .command("exec")
+    .description("Run the project's debug executable")
+    .action(runExec);
+program
+    .command("build")
+    .description("Build the project binaries")
+    .action(runBuild);
+program.parse();
