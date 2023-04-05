abe9ad1f0e019aeab8736bb1a0697bae114bda0f
Jesse Koldewijn <jesse@iungo.nl>
2023-04-05 09:38:42 +0200

    fix: get projectname from CMakeLists file instead of dir name

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,14 +1,48 @@
 import { exec } from "node:child_process";
 import { platform } from "node:process";
 import path from "path";
+import fs from "fs";
 
-export const runExec = () => {
-	const projectName = path.basename(path.resolve(process.cwd()));
+export const runExec = async () => {
+	// const projectDirName = path.basename(path.resolve(process.cwd()));
+	const projectPathName = path.resolve(process.cwd());
+	const projectName = [];
+
+	try {
+		const CmakeFile = await fs.readFileSync(
+			path.resolve(String(`${projectPathName}/CMakeLists.txt`)),
+			"utf-8"
+		);
+		if (CmakeFile) {
+			const projectCmakefileArray = CmakeFile.split("\n");
+			projectCmakefileArray.map(async (line) => {
+				if (line.replaceAll("\r", "").includes("project(")) {
+					const projNameCleaned = line
+						.replaceAll("\r", "")
+						.replaceAll("project(", "")
+						.replaceAll(")", "")
+						.split(" ");
+					projectName.push(projNameCleaned[0]);
+				}
+			});
+		} else {
+			console.log("error: couldn't find CMakeLists.txt file.");
+		}
+	} catch (err) {
+		console.log(`error: ${err}`);
+	}
+
+	if (!projectName || projectName.length !== 1) {
+		console.log(
+			"error: couldn't find project name in CMakeLists.txt file."
+		);
+		return;
+	}
 
 	if (platform == "win32") {
 		try {
 			const execFilePath = path.resolve(
-				`./build/Debug/${projectName}.exe`
+				`./build/Debug/${projectName[0]}.exe`
 			);
 			exec(`${execFilePath}`, (error, stdout, stderr) => {
 				if (error) {
@@ -23,12 +57,12 @@ export const runExec = () => {
 			});
 		} catch (err) {
 			console.log(
-				`error: make sure your cmake project name is the same as your baseDir (${projectName}`
+				`error: make sure your cmake project name is the same as your baseDir (${projectName[0]}`
 			);
 		}
 	} else if (platform == "linux") {
 		try {
-			const execFilePath = path.resolve(`./build/${projectName}`);
+			const execFilePath = path.resolve(`./build/${projectName[0]}`);
 			exec(`sh ${execFilePath}`, (error, stdout, stderr) => {
 				if (error) {
 					console.log(`error: ${error.message}`);
@@ -42,7 +76,7 @@ export const runExec = () => {
 			});
 		} catch (err) {
 			console.log(
-				`error: make sure your cmake project name is the same as your baseDir (${projectName}`
+				`error: make sure your cmake project name is the same as your baseDir (${projectName[0]}`
 			);
 		}
 	} else {
